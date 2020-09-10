9377232f80adc47e11b1a2ca2c201b490a6ec220
Xander Jones <xander@bugsnag.com>
2020-09-10 11:22:21 +0100

    Replace formula single quotes with double quotes

--- Formula/bugsnag-dsym-upload.rb
+++ Formula/bugsnag-dsym-upload.rb
@@ -1,8 +1,8 @@
 class BugsnagDsymUpload < Formula
   desc "Commands for uploading files to Bugsnag via the upload APIs"
-  homepage 'https://docs.bugsnag.com/api/dsym-upload'
+  homepage "https://docs.bugsnag.com/api/dsym-upload"
   url "https://github.com/bugsnag/bugsnag-dsym-upload/archive/v2.0.0.tar.gz"
-  head 'https://github.com/bugsnag/bugsnag-dsym-upload'
+  head "https://github.com/bugsnag/bugsnag-dsym-upload"
   sha256 "182e99f49ae6915c61d09cd705d67d304f6cf9f34fe4abb8c0b9668944899b98"
   license "MIT"
   bottle :unneeded
