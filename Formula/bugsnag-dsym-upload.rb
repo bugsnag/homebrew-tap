c9d9c825f4de2e1772ed74b67dfb699399d47756
matth-bugsnag <113024072+matth-bugsnag@users.noreply.github.com>
2023-02-08 16:39:33 +0000

    Updating fastlane homebrew version to 2.3.0

--- Formula/bugsnag-dsym-upload.rb
+++ Formula/bugsnag-dsym-upload.rb
@@ -1,8 +1,8 @@
 class BugsnagDsymUpload < Formula
   desc "Commands for uploading files to Bugsnag via the upload APIs"
   homepage "https://docs.bugsnag.com/api/dsym-upload"
-  url "https://github.com/bugsnag/bugsnag-dsym-upload/archive/refs/tags/v2.2.1.tar.gz"
-  sha256 "bf6517b0d437adc167bc42b97376b46f41f7773ac0faaa9ed8697ce0082a5277"
+  url "https://github.com/bugsnag/bugsnag-dsym-upload/archive/refs/tags/v2.3.0.tar.gz"
+  sha256 "f3b063a023be5db21807ba6bacbc2999f904db046ad267b888d23e4ee0adf165"
   license "MIT"
   head "https://github.com/bugsnag/bugsnag-dsym-upload"
 
